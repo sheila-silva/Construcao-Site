f9c02badc19703f5ca1d0276ead22d95dd6b1474
Sheila Silva <loreagate@gmail.com>
2024-12-11 08:46:20 -0300

    segunda correcao

--- contato.js
+++ contato.js
@@ -1,3 +1,8 @@
-// Contato JS //
+function classToggle() {
+    var el = document.querySelector('.icon-cards__content');
+    el.classList.toggle('step-animation');
+  }
+  
+  document.querySelector('#toggle-animation').addEventListener('click', classToggle);
 
-    
+  
